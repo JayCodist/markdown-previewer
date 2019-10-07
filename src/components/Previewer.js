549836a97926c2cce1e834cba8add11411eb245c
Johnson Ubaezuonu <jaycodist@gmail.com>
2019-10-08 00:24:51 +0100

    bug fix

--- src/components/Previewer.js
+++ src/components/Previewer.js
@@ -3,7 +3,11 @@ import '../styles/Previewer.css';
 
 const Previewer = props =>
 {
-	
+	React.useEffect(() => 
+	{
+		for (let item of document.querySelectorAll("#preview a"))
+			item.setAttribute("target", "_blank");
+	})
 	return (
 		<div id="preview-container">
 			<div id="preview-header">Previewer</div>
