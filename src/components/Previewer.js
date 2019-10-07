f590fd00482c48135a265512edb563fb2191635f
Johnson Ubaezuonu <jaycodist@gmail.com>
2019-10-08 00:28:50 +0100

    refactorings

--- src/components/Previewer.js
+++ src/components/Previewer.js
@@ -11,11 +11,8 @@ const Previewer = props =>
 	return (
 		<div id="preview-container">
 			<div id="preview-header">Previewer</div>
-			{/*<div id="preview">
-				{props.output}
-			</div>*/}
 			<div id="preview" dangerouslySetInnerHTML={{__html: props.output}}>
-						</div>
+			</div>
 		</div>);;
 }
 
